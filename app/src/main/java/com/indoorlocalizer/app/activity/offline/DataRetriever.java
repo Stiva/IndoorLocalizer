540bfc279e1c8f0df4b4a25d2d064ac656a195aa
Stiva <fstivani@gmail.com>
2014-06-07 12:06:03 +0200

    Reference point and hits db show fix

--- app/src/main/java/com/indoorlocalizer/app/activity/offline/DataRetriever.java
+++ app/src/main/java/com/indoorlocalizer/app/activity/offline/DataRetriever.java
@@ -3,13 +3,12 @@ package com.indoorlocalizer.app.activity.offline;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.indoorlocalizer.app.R;
 
@@ -22,24 +21,17 @@ public class DataRetriever extends ActionBarActivity {
         setContentView(R.layout.activity_data_retriever);
         final Button dataRetrieveButton=(Button) findViewById(R.id.data_retriving_button);
         final EditText rpValueText=(EditText)findViewById(R.id.rp_id_editText);
-
-        rpValueText.addTextChangedListener(new TextWatcher() {
-
-            public void afterTextChanged(Editable s) {
-                // you can call or do what you want with your EditText here
-                rpValue=Integer.parseInt(s.toString());
-            }
-
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-            public void onTextChanged(CharSequence s, int start, int before, int count) {}
-        });
         scanService=new Intent(this.getApplicationContext(),ScannerService.class);
-        scanService.putExtra("rpID",rpValue);
         dataRetrieveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                scanReferencePoint();
+                if(!rpValueText.getText().toString().isEmpty()) {
+                    rpValue=Integer.parseInt(rpValueText.getText().toString());
+                    scanService.putExtra("rpID",rpValue);
+                    scanReferencePoint();
+                } else {
+                    Toast.makeText(getApplicationContext(),"You've to select an ID for RP",Toast.LENGTH_LONG).show();
+                }
             }
         });
         final Button stopRetrieveButton=(Button) findViewById(R.id.stop_rp_button);
